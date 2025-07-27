d3b77a626d3b94bb16cc2fdcd33439f0e366085f
Shiva-Shakthi02 <shakuma1330@gmail.com>
2025-07-27 08:37:26 -0400

    Adding temporary change to Github

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -11,6 +11,12 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
+    # path for logout
+    path('logout', views.logout_request, name='logout'),
+
+    # path for register
+    path('Register', views.registration, name='Register'),
+
     # path for dealer reviews view
 
     # path for add a review view
